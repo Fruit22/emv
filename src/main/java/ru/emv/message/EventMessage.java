f5378f97126a3640ead032ccfbd108099f9d1999
fruit <fruit22@mail.ru>
2017-04-18 00:21:08 +0300

    todo for EventMessage

--- src/main/java/ru/emv/message/EventMessage.java
+++ src/main/java/ru/emv/message/EventMessage.java
@@ -3,6 +3,7 @@ package ru.emv.message;
 /**
  * Created by fruit on 17.04.2017.
  */
+// TODO: 18.04.2017 продумать структуру сообщений 
 public class EventMessage {
     private final int id;
     private final String message;
