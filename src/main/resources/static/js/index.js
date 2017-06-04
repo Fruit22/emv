45e76c9a51236cb17da859470edc5006364904d8
gay <max.khabrat@gmail.com>
2017-06-05 00:09:34 +0300

    Тесты ридера

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,17 +1,26 @@
-
 $(document).ready(function () {
-    var button = document.getElementById('btn1');
-    button.addEventListener('click', function () {
-        $.ajax({
-            url: "readerCommands/actCommand",
-            type: "POST"
-        }).done(function (result) {
-            if (result) {
-                $("#test-result").text("Тест пройден!");
-            } else {
-                $("#test-result").text("Тест провален!");
-            }
+    var actButton = document.getElementById('act-signal');
+    var stopButton = document.getElementById('stop-signal');
+    var cleanButton = document.getElementById('clean-signal');
+
+    addTestToButton(actButton, "readerCommands/actCommand");
+    addTestToButton(stopButton, "readerCommands/stopCommand");
+    addTestToButton(cleanButton, "readerCommands/cleanCommand");
+
+    // Добавление тестов
+    function addTestToButton(button, testUrl) {
+        button.addEventListener('click', function () {
+            $.ajax({
+                url: testUrl,
+                type: "POST"
+            }).done(function (result) {
+                if (result) {
+                    $("#test-result").text("Тест пройден!");
+                } else {
+                    $("#test-result").text("Тест провален!");
+                }
+            });
         });
-    });
+    }
 
 });
